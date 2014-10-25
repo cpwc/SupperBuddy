674f35ccce7737b4d766f802405ed58e37d3e14e
Wei Cheng <calvinpohwc@gmail.com>
2014-10-25 16:21:03 +0800

    Changed URL to assets.

--- application/views/caterer_login.php
+++ application/views/caterer_login.php
@@ -15,21 +15,21 @@
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
 <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
 <link href="/public/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css"/>
+<link href="/public/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+<link href="/public/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="/public/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+<link href="/public/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css"/>
 <!-- END GLOBAL MANDATORY STYLES -->
 <!-- BEGIN PAGE LEVEL STYLES -->
-<link href="../../assets/global/plugins/select2/select2.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/admin/pages/css/login.css" rel="stylesheet" type="text/css"/>
+<link href="/public/plugins/select2/select2.css" rel="stylesheet" type="text/css"/>
+<link href="/public/css/login.css" rel="stylesheet" type="text/css"/>
 <!-- END PAGE LEVEL SCRIPTS -->
 <!-- BEGIN THEME STYLES -->
-<link href="../../assets/global/css/components.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/global/css/plugins.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/admin/layout/css/layout.css" rel="stylesheet" type="text/css"/>
-<link id="style_color" href="../../assets/admin/layout/css/themes/default.css" rel="stylesheet" type="text/css"/>
-<link href="../../assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
+<link href="/public/css/components.css" rel="stylesheet" type="text/css"/>
+<link href="/public/css/plugins.css" rel="stylesheet" type="text/css"/>
+<link href="/public/css/layout.css" rel="stylesheet" type="text/css"/>
+<link id="style_color" href="/public/css/themes/default.css" rel="stylesheet" type="text/css"/>
+<link href="/public/css/custom.css" rel="stylesheet" type="text/css"/>
 <!-- END THEME STYLES -->
 <link rel="shortcut icon" href="favicon.ico"/>
 </head>
