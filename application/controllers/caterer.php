de046b638623f3f6b726040302c1ea15a66c52a2
Wei Cheng <calvinpohwc@gmail.com>
2014-10-25 16:48:35 +0800

    Caterer Controller.

---
+++ application/controllers/caterer.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Caterer extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('caterer_login');
+	}
+
+	public function login()
+	{
+	    if ($this->input->server('REQUEST_METHOD') === 'POST') {
+            if ($this->_login_submit_validate() === false) {
+                $this->login();
+                return;
+            }
+            redirect('/');
+        } else {
+            redirect('/');
+        }
+	}
+
+    private function _login_submit_validate()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|callback_authenticate|xss_clean');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        $this->form_validation->set_message('authenticate', 'Invalid email or password. Please try again.');
+
+        return $this->form_validation->run();
+    }
+
+    public function authenticate()
+    {
+        return false;
+    }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
--- application/views/caterer_login.php
+++ application/views/caterer_login.php
@@ -47,19 +47,33 @@
 <!-- BEGIN LOGIN -->
 <div class="content">
 	<!-- BEGIN LOGIN FORM -->
-	<form class="login-form" action="index.html" method="post">
+	<form class="login-form" action="caterer/login" method="post">
 		<h3 class="form-title">Caterer Login</h3>
 		<div class="alert alert-danger display-hide">
 			<button class="close" data-close="alert"></button>
 			<span>
 			Enter any username and password. </span>
 		</div>
+
+		<?php if (validation_errors()) { ?>
+			<div class="alert alert-danger">
+				<button class="close" data-close="alert"></button>
+				<span><?php echo validation_errors(); ?></span>
+			</div>
+		<?php } ?>
+
+		<?php if ($this->session->flashdata('message')) { ?>
+			<div class="alert alert-warning">
+				<button class="close" data-close="alert"></button>
+				<span><?php echo $this->session->flashdata('message'); ?></span>
+			</div>
+		<?php } ?>
 		<div class="form-group">
 			<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
 			<label class="control-label visible-ie8 visible-ie9">Username</label>
 			<div class="input-icon">
 				<i class="fa fa-user"></i>
-				<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+				<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email"/>
 			</div>
 		</div>
 		<div class="form-group">
@@ -190,7 +204,7 @@
 <!-- END LOGIN -->
 <!-- BEGIN COPYRIGHT -->
 <div class="copyright">
-	 2014 &copy; NUS supperbuddy.
+	 2014 &copy; NUSSupperBuddy.
 </div>
 <!-- END COPYRIGHT -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
