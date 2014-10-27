3f59d6cd5fd55190531a710d158f83aa7ab5fd6a
tgsCalvin <calvinsim001@hotmail.com>
2014-10-27 18:55:20 +0800

    Debugging Student

--- application/controllers/student.php
+++ application/controllers/student.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Caterer extends CI_Controller {
+class Student extends CI_Controller {
 
 	public function index()
 	{
