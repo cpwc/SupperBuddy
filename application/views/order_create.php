af577af026dc8f4c46f1b791a452718277bb3a75
tgsCalvin <calvinsim001@hotmail.com>
2014-10-28 16:50:43 +0800

    Debugging

--- application/views/order_create.php
+++ application/views/order_create.php
@@ -52,7 +52,13 @@
 <div class="page-header navbar navbar-fixed-top">
 	<!-- BEGIN HEADER INNER -->
 	<div class="page-header-inner">
-		
+		<!-- BEGIN LOGO -->
+		<div class="page-logo">
+			<a href="index.html">
+			<img src="/public/img/logo.png" alt="logo" class="logo-default"/>
+			</a>
+		</div>
+		<!-- END LOGO -->
 		<!-- BEGIN RESPONSIVE MENU TOGGLER -->
 		<a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
 		</a>
