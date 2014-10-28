9756722e6da7065c82b47a0cc8c01a54d2e5c1d1
Wei Cheng <calvinpohwc@gmail.com>
2014-10-28 16:31:30 +0800

    Added support for order listings.

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -4,7 +4,12 @@ class Order extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('order_view');
+		$sql = "SELECT order.id, caterer.name as `caterer_name`, student.name as `student_name`, residence.name as `residence_name`, order.created_at, order.updated_at, order.status FROM `order`, `caterer`, `student`, `residence` WHERE order.caterer_id = caterer.id AND residence.id = student.residence_id AND order.created_by = student.matric_no";
+		$orders = $this->db->query($sql);
+
+		$data['orders'] = $orders->result();
+
+		$this->load->view('order_view', $data);
 	}
 
 }
