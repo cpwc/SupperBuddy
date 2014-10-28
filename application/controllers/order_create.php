a4e55f3a60f2881a8ab5be304d19ed288f4ee91a
tgsCalvin <calvinsim001@hotmail.com>
2014-10-28 16:43:22 +0800

    Added Order Create.

---
+++ application/controllers/order_create.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class OrderCreate extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('order_create');
+	}
+
+}
+
+/* End of file order.php */
+/* Location: ./application/controllers/order.php */
