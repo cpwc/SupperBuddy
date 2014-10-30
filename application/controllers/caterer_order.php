309f97b79ae938913713fa8d1a9dac9ef09092cd
tgsCalvin <calvinsim001@hotmail.com>
2014-10-30 14:36:28 +0800

    Renamed `CatererOrder` to `Catererorder`

--- application/controllers/caterer_order.php
+++ application/controllers/caterer_order.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class CatererOrder extends CI_Controller {
+class Catererorder extends CI_Controller {
 
 	public function index()
 	{
