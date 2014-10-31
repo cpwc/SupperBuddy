40bff524858355730552a7c1860e1fca1a983cab
tgsCalvin <calvinsim001@hotmail.com>
2014-10-31 15:33:15 +0800

    debug

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -4,6 +4,17 @@ class Order extends CI_Controller {
 
 	public function index()
 	{
+		$usenet = $this->session->userdata('usenet');
+		$usenet_id = $usetnet['matric_no'];
+
+		if (!$usenet_id) {
+			redirect('/student/login');
+			return;
+		} else {
+			redirect('/order');
+			return;
+		}
+		
 		$sql = "SELECT order.id, caterer.name as `caterer_name`, student.name as `student_name`, residence.name as `residence_name`, order.created_at, order.updated_at, order.status FROM `order`, `caterer`, `student`, `residence` WHERE order.caterer_id = caterer.id AND residence.id = student.residence_id AND order.created_by = student.matric_no";
 		$orders = $this->db->query($sql);
 
