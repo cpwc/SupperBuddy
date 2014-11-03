7b40cb50fe09a6e5fc48d8bfd02d230cfc7dc701
tgsCalvin <calvinsim001@hotmail.com>
2014-11-03 18:54:18 +0800

    Debug

--- application/views/caterer_order_view.php
+++ application/views/caterer_order_view.php
@@ -525,24 +525,22 @@
 								<div class="form-body">
 									<div class="row search-form-default">
 										<div class="col-md-12">
-											<form action="#">
-												<div class="input-group">
+											
+												<div class="input-group" style="width:50%">
 													<div class="input-cont">
 														<input type="text" placeholder="Search by Hall.." class="form-control"/>
 													</div>
 												</div>
-											</form>
+										
 										</div>
 									</div>
 									<div class="row search-form-default">
 										<div class="col-md-12">
-											<form action="#">
 												<div class="input-group">
-													<div class="input-cont">
-														<input type="text" placeholder="Search by Status.." class="form-control"/>
+													<div class="input-cont" style="width:50%">
+														<input type="text" placeholder="Search by Hall/Status.." class="form-control"/>
 													</div>
 												</div>
-											</form>
 										</div>
 									</div>
 								</div>
