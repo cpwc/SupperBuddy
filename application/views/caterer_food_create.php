7ee5a42bf6f7dd4714cc21dcc9bcaf7fb29e7afb
Wei Cheng <calvinpohwc@gmail.com>
2014-11-04 17:19:25 +0800

    Updated view for food_create

--- application/views/caterer_food_create.php
+++ application/views/caterer_food_create.php
@@ -28,59 +28,52 @@
 				</ul>
 			</div>
 			<!-- END PAGE HEADER-->
-			<?php echo form_open('food/create'); ?>
-				<!-- BEGIN PAGE CONTENT-->
-				<div class="row">
-					<div class="col-md-12 col-sm-12">
-						<div class="portlet blue-hoki box">
-							<div class="portlet-title">
-								<div class="caption">
-									<i class="fa fa-cogs"></i>Food Information
-								</div>
+			<?php echo form_open('food/create', array('class' => 'form-horizontal')); ?>
+			<!-- BEGIN PAGE CONTENT-->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="portlet blue-hoki box">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-cogs"></i> Food Information
 							</div>
-							<div class="portlet-body">
-								<div class="row static-info">
-									<div class="col-md-2 name">
+						</div>
+						<div class="portlet-body form">
+							<div class="form-body">
+								<div class="form-group">
+									<div class="col-md-3 control-label">
 										Food Name:
 									</div>
-									<div class="col-md-7 value">
+									<div class="col-md-9">
 										<div class="input-icon right">
 											<input type="text" class="form-control" name="name" id="food_name">
 										</div>
-										<div class="help-block">
-												food name
-										</div>
-
 									</div>
 								</div>
-								<div class="row static-info">
-									<div class="col-md-2 name">
-										 Price:
+								<div class="form-group">
+									<div class="col-md-3 control-label">
+										Price:
 									</div>
-									<div class="col-md-7 value">
-										 <div class="input-icon right">
+									<div class="col-md-9">
+										<div class="input-icon right">
 											<input type="text" class="form-control" name="price" id="food_price">
 										</div>
-										<div class="help-block">
-												food price
-										</div>
-
 									</div>
 								</div>
-								<div class="row static-info">
-									<div class="col-md-2 name">
-										
-									</div>
-									<div class="col-md-7 value">
-										<input type="submit" class="btn default" value="Add Food">
+							</div>
+							<div class="form-actions">
+								<div class="row">
+									<div class="col-md-offset-3 col-md-9">
+										<input type="submit" class="btn green" value="Add Food">
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<!-- END PAGE CONTENT-->
-				<!-- <input type="hidden" name="caterer_id" value="" id="caterer-id"> -->
+			</div>
+			<!-- END PAGE CONTENT-->
+			<!-- <input type="hidden" name="caterer_id" value="" id="caterer-id"> -->
 			<?php echo form_close(); ?>
 		</div>
 	</div>
